e2d8853c2d4b399eb6ebcf13e63aa348c86dd0ae
varun-madhu <raidenlolt@gmail.com>
2017-04-19 14:46:40 +0530

    Expanded functionality of Rune monobehaviour.

--- Project/RuneGame/Assets/Scripts/Rune.cs
+++ Project/RuneGame/Assets/Scripts/Rune.cs
@@ -23,6 +23,23 @@ public class Rune : MonoBehaviour
     /// </summary>
     public int Column { get; set; }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Type { get; set; }
+
+    #endregion
+
+    #region Contructors
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public Rune()
+    {
+        Bonus = BonusType.None;
+    }
+
     #endregion
 
     #region Unity Lifecycle
@@ -43,7 +60,50 @@ public class Rune : MonoBehaviour
 
     #region Public Methods
 
+    /// <summary>
+    /// check if a rune is of the same type as this.
+    /// </summary>
+    /// <param name="otherRune"></param>
+    /// <returns></returns>
+    public bool IsSameType(Rune otherRune)
+    {
+        if ((otherRune == null) || !(otherRune is Rune))
+            throw new System.ArgumentException("otherRune");
 
+        return string.Compare(Type, (otherRune as Rune).Type) == 0;
+    }
+
+    /// <summary>
+    /// assign the rune.
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    public void Assign(string type, int row, int column)
+    {
+        if (string.IsNullOrEmpty(type))
+            throw new System.ArgumentException("type");
+
+        Column = column;
+        Row = row;
+        Type = type;
+    }
+
+    /// <summary>
+    /// swap position of runes.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    public static void SwapColumnRow(Rune a, Rune b)
+    {
+        int temp = a.Row;
+        a.Row = b.Row;
+        b.Row = temp;
+
+        temp = a.Column;
+        a.Column = b.Column;
+        b.Column = temp;
+    }
 
     #endregion
 
