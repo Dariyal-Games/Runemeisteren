3bcd15ea71558a1b8d3572a92aa5229f90be22af
varun-madhu <raidenlolt@gmail.com>
2017-04-19 15:03:02 +0530

    More changes.

--- Project/RuneGame/Assets/Scripts/Data/Constants.cs
+++ Project/RuneGame/Assets/Scripts/Data/Constants.cs
@@ -1,6 +1,14 @@
-﻿public static class Constants
+﻿namespace Dariyal.Runes
 {
-    public static readonly int Rows = 12;
-    public static readonly int Columns = 8;
-    public static readonly float AnimationDuration = 0.2f;
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Constants
+    {
+        public static readonly int Rows = 12;
+        public static readonly int Columns = 8;
+        public static readonly float AnimationDuration = 0.2f;
+
+        public static readonly float OpacityAnimationFrameDelay = 0.05f;
+    }
 }
---
+++ Project/RuneGame/Assets/Scripts/Utilities/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dariyal.Runes
+{
+    public static class Utilities
+    {
+        public static IEnumerator AnimatePotentialMatches(IEnumerable<GameObject> potentialMatches)
+        {
+            for (float i = 1f; i >= 0.3f; i -= 0.1f)
+            {
+                foreach (var item in potentialMatches)
+                {
+                    //Do Something
+                }
+
+                yield return new WaitForSeconds(Constants.OpacityAnimationFrameDelay);
+            }
+        }
+
+        public static bool AreVerticalOrHorizontalNeighbours(Rune a, Rune b)
+        {
+            return (a.Column == b.Column || a.Row == b.Row && Mathf.Abs(a.Column - b.Column) <= 1 && Mathf.Abs(a.Row - b.Row) <= 1);
+        }
+    }
+}
