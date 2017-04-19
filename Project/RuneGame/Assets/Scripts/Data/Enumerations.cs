289c7a5eaf64643d0732faddbcd3442c406aff59
varun-madhu <raidenlolt@gmail.com>
2017-04-19 14:26:26 +0530

    Started

---
+++ Project/RuneGame/Assets/Scripts/Data/Enumerations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+/// <summary>
+/// 
+/// </summary>
+namespace Dariyal.Runes.Enumerations
+{
+    /// <summary>
+    /// Flags to indicate bonus type.
+    /// </summary>
+    [Flags]
+    public enum BonusType
+    {
+        None,
+        DestroyWholeRowColumn,
+        DestroySurrounding,
+    }
+
+    /// <summary>
+    /// Extension methods to enable easy flag checks
+    /// </summary>
+    public static class BonusTypeExtensions
+    {
+        /// <summary>
+        /// Check if the bonus type contains Destroy Whole row or column flag.
+        /// </summary>
+        /// <param name="bt">The bonus type to check.</param>
+        /// <returns>Check result.</returns>
+        public static bool ContainsDestroyWholeRowColumn(BonusType bt)
+        {
+            return (bt & BonusType.DestroyWholeRowColumn) == BonusType.DestroyWholeRowColumn;
+        }
+
+        /// <summary>
+        /// Check if the bonus type contains Destroy surrounding flag.
+        /// </summary>
+        /// <param name="bt">The bonus type to check.</param>
+        /// <returns>Check result.</returns>
+        public static bool ContainsDestroySurrounding(BonusType bt)
+        {
+            return (bt & BonusType.DestroySurrounding) == BonusType.DestroySurrounding;
+        }
+    }
+
+    /// <summary>
+    /// States in the game.
+    /// </summary>
+    public enum GameState
+    {
+        None,
+        SelectionStarted,
+        Animating,
+    }
+}
---
+++ Project/RuneGame/Assets/Scripts/Rune.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using Dariyal.Runes.Enumerations;
+
+/// <summary>
+/// Rune base class.
+/// </summary>
+public class Rune : MonoBehaviour
+{
+    #region Properties and Members
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public BonusType Bonus { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public int Row { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public int Column { get; set; }
+
+    #endregion
+
+    #region Unity Lifecycle
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #endregion
+
+    #region Public Methods
+
+
+
+    #endregion
+
+}
